ebbbf2297007fdcb6b6565c09cd203afe119eeab
Rob <robseso66@gmail.com>
2024-03-17 00:16:15 +1000

    Fixed small bug with check_log_size

--- intelpy/logging/logrotate.py
+++ intelpy/logging/logrotate.py
@@ -2,11 +2,11 @@ import os
 
 
 def check_log_size(log_file, max_size=5242880):
-    file_info = os.stat(log_file)
-    if file_info.st_size > max_size:
-        return True
-    else:
-        return False
+    if os.path.exists(log_file):
+        file_info = os.stat(log_file)
+        if file_info.st_size > max_size:
+            return True
+    return False
 
 
 def rotate_log_file(log_file_full_path, to_name):
