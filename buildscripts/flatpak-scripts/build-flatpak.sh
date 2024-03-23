5ef71cf06a979462d985b66af58b238d196ec501
Rob <robseso66@gmail.com>
2024-03-24 00:19:53 +1000

    Yet more flatpak changes

--- buildscripts/flatpak-scripts/build-flatpak.sh
+++ buildscripts/flatpak-scripts/build-flatpak.sh
@@ -4,7 +4,8 @@ echo "*** Compiling with Pyinstaller ***"
 #./build.sh   #commented out so we don't rebuild while testing this script
 
 echo "*** Building Flatpak ***"
-flatpak-builder build-dir io.github.riftr.intelpy.yml
+flatpak-builder --force-clean --user build-dir io.github.riftr.intelpy.yml
+#flatpak run org.flatpak.Builder --force-clean --sandbox --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo builddir io.github.riftr.intelpy.yml
 
 #echo "*** Testing Flatpak ***"
 #flatpak-builder --user --install --force-clean build-dir io.github.riftr.intelpy.yml
