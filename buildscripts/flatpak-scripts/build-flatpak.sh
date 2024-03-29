c00f4e499752a49bf7bfe4212dea3316e7f3c487
Riftr <9170186+Riftr@users.noreply.github.com>
2024-03-29 17:45:06 +1000

    Flatpak successfully builds and runs now. Removed Wayland access because it doesn't work with this Runtime.

--- buildscripts/flatpak-scripts/build-flatpak.sh
+++ buildscripts/flatpak-scripts/build-flatpak.sh
@@ -4,4 +4,5 @@ echo "** Building Flatpak **"
 flatpak-builder --force-clean build-dir ./io.github.riftr.intelpy.yml
 
 echo "** Running Flatpak **"
+set QT_DEBUG_PLUGINS = 1
 flatpak-builder --run build-dir ./io.github.riftr.intelpy.yml run.sh
