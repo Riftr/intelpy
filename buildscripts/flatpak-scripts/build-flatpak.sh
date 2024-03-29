5c6dbb1bb5ee47f797dc60c6b48bdc800a51f299
Riftr <9170186+Riftr@users.noreply.github.com>
2024-03-29 17:07:00 +1000

    Reenabled test run of flatpak. Builds OK but can't see PyQt libs.

--- buildscripts/flatpak-scripts/build-flatpak.sh
+++ buildscripts/flatpak-scripts/build-flatpak.sh
@@ -4,4 +4,4 @@ echo "** Building Flatpak **"
 flatpak-builder --force-clean build-dir ./io.github.riftr.intelpy.yml
 
 echo "** Running Flatpak **"
-#flatpak-builder --run build-dir ./io.github.riftr.intelpy.yml run.sh
+flatpak-builder --run build-dir ./io.github.riftr.intelpy.yml run.sh
