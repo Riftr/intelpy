895171f7e52ec5c479e51b263eac6e05678f73de
Riftr <9170186+Riftr@users.noreply.github.com>
2024-03-31 16:44:54 +1000

    Updated flatpak build script, desktop file and added 128x128 svg icon

--- buildscripts/flatpak-scripts/build-flatpak.sh
+++ buildscripts/flatpak-scripts/build-flatpak.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo "** Validating .desktop **"
+desktop-file-validate io.github.riftr.intelpy.desktop
+echo "✔ if no output = validated"
+
+echo "** Validating Metainfo **"
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder appstream io.github.riftr.intelpy.metainfo.xml
+
 echo "** Building Flatpak **"
-flatpak-builder --force-clean build-dir ./io.github.riftr.intelpy.yml
+#flatpak-builder --force-clean build-dir /../../io.github.riftr.intelpy.yml
+flatpak run org.flatpak.Builder --force-clean --sandbox --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo build-dir ../../io.github.riftr.intelpy.yml
+
 
 echo "** Running Flatpak **"
-set QT_DEBUG_PLUGINS = 1
-flatpak-builder --run build-dir ./io.github.riftr.intelpy.yml run.sh
+#set QT_DEBUG_PLUGINS = 1
+#flatpak-builder --run build-dir ../../io.github.riftr.intelpy.yml run.sh
