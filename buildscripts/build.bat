ef1ad591f3a8d1dfa1f5c57c55e2cb313ab4e3b9
Rob <9170186+Riftr@users.noreply.github.com>
2024-03-31 18:34:32 +1000

    Minor updates to Windows build script
    Minor updates to Windows build script

--- buildscripts/build.bat
+++ buildscripts/build.bat
@@ -1,5 +1,2 @@
 pyinstaller ..\intelpy.py --noconsole --onefile --add-data ..\intelpy\resources;resources --noconfirm --icon=..\intelpy\gui\goodpie2.ico
-mkdir ..\dist
-cd ..\dist
-xcopy /I ..\intelpy\resources .\resources\
-cd..
+xcopy /I ..\intelpy\resources .\dist\resources\
